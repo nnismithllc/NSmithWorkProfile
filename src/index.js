900d3b2088df44ec3c1264fefb0afd5cb587718b
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-11-05 17:43:02 -0500

    added bootstrap to index.js - not a working file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+React.render(<App />, document.getElementById('root'));
 
 
 // If you want to start measuring performance in your app, pass a function
