6e998902f1135bda034c1c67c1bfb23a6accdda5
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-11-06 01:54:09 -0500

    removed bn file download for layout - not working

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 import App from './App';
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
-import 'bootstrap-css-only/css/bootstrap.min.css'; 
-import 'mdbreact/dist/css/mdb.css';
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
