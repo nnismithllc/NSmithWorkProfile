762ec32a734783fcac1f43163d3aa45353e88254
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-11-08 02:50:49 -0500

    updated mail contact information- working app not working email form

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,151 +1,144 @@
 // Imports from App.js Page to Run Contact Function
-import React from "react";
-import {
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBIcon,
-  MDBBtn,
-  MDBInput,
-} from "mdbreact";
-
+import React from 'react';
 import emailjs from 'emailjs-com';
+import './Contact.js';
+import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon, MDBBtn, MDBInput } from "mdbreact";
 
-// Function for Contact Page Information
-function Contact() {
-  return (
-    <section className="contact-section my-5">
-      <MDBCard>
-        <MDBRow>
-          <MDBCol lg="8">
-            <MDBCardBody className="form"href={sendEmail}>
-              <h3 className="mt-4">
-                <MDBIcon icon="envelope" className="pr-2" />
-                Send Me an Email:
-              </h3>
-              <MDBRow>
-                <MDBCol md="6">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="text"
-                      id="form-contact-name"
-                      label="Your name"/>
-                  </div>
-                </MDBCol>
-                <MDBCol md="6">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="text"
-                      id="form-contact-email"
-                      label="Your email"/>
-                  </div>
-                </MDBCol>
-              </MDBRow>
-              <MDBRow>
-                <MDBCol md="6">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="text"
-                      id="form-contact-phone"
-                      label="Your phone"/>
-                  </div>
-                </MDBCol>
-                <MDBCol md="6">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="text"
-                      id="form-contact-company"
-                      label="Your company"/>
-                  </div>
-                </MDBCol>
-              </MDBRow>
-              <MDBRow>
-                <MDBCol md="12">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="textarea"
-                      id="form-contact-message"
-                      label="Your message"/>
-                    <MDBBtn rounded color="blue" onClick={sendEmail}>
-                    <MDBIcon icon="paper-plane"/>
-                  
-                    </MDBBtn>
-                  </div>
-                </MDBCol>
-              </MDBRow>
-            </MDBCardBody>
-          </MDBCol>
-          <MDBCol lg="4">
-            <MDBCardBody className="contact text-center h-100 black-text">
-              <h3 className="my-4 pb-2">Contact information</h3>
-              <ul className="text-lg-left list-unstyled ml-4">
-                <li>
-                  <p>
-                    <MDBIcon icon="map-marker-alt" className="pr-2" />
-                    Michigan, 48184 USA
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <MDBIcon icon="phone" className="pr-2" />
-                    313-457-3323
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <MDBIcon icon="envelope" className="pr-2" />
-                    nnismithllc@gmail.com
-                  </p>
-                </li>
-              </ul>
-              <hr className="hr-light my-4" />
-              <ul className="list-inline text-center list-unstyled">
-                <li className="list-inline-item">
-                  <a
-                    href="https://www.linkedin.com/in/nintia-smith/"
-                    className="p-2 fa-lg w-ic"
-                  >
-                    <MDBIcon fab icon="linkedin" />
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a
-                    href="https://github.com/nnismithllc"
-                    className="p-2 fa-lg w-ic"
-                  >
-                    <MDBIcon fab icon="github-square" />
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a
-                    href="mailto:nnismithllc@gmail.com"
-                    className="p-2 fa-lg w-ic"
-                  >
-                    <MDBIcon fab icon="envelope-open" />
-                  </a>
-                </li>
-              </ul>
-            </MDBCardBody>
-          </MDBCol>
-        </MDBRow>
-      </MDBCard>
-    </section>
-  );
-}
+export default function Contact() {
 
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+    emailjs.sendForm('gmail', 'template_8xf3fh5', e.target, 'user_C7KCkTN3yY1VzbTJMeAJg')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+      e.target.reset()
   }
 
+  return (
+    <div>
+      <div className="container">
+      <form onClick={sendEmail} id='form'>
+      <section className="contact-section my-5">
+        <MDBCard>
+          <MDBRow>
+            <MDBCol lg="8">
+              <MDBCardBody className="form">
+                <h3 className="mt-4">
+                  <MDBIcon icon="envelope" className="pr-2" />
+                  Send Me an Email:
+                </h3>
+                <MDBRow>
+                  <MDBCol md="6">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="text"
+                        id="form-contact-name"
+                        label="Your name"
+                      />
+                    </div>
+                  </MDBCol>
+                  <MDBCol md="6">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="text"
+                        id="form-contact-email"
+                        label="Your email"
+                      />
+                    </div>
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                  <MDBCol md="6">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="text"
+                        id="form-contact-phone"
+                        label="Your phone"
+                      />
+                    </div>
+                  </MDBCol>
+                  <MDBCol md="6">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="text"
+                        id="form-contact-company"
+                        label="Your company"
+                      />
+                    </div>
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                  <MDBCol md="12">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="textarea"
+                        id="form-contact-message"
+                        label="Your message"
+                      />
+                      <button onClick ={sendEmail} rounded color="blue" id='form'>
+                        <MDBIcon icon="paper-plane" />
+                      </button>
+                    </div>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCol>
+            <MDBCol lg="4">
+              <MDBCardBody className="contact text-center h-100 black-text">
+                <h3 className="my-4 pb-2">Contact information</h3>
+                <ul className="text-lg-left list-unstyled ml-4">
+                  <li>
+                    <p>
+                      <MDBIcon icon="map-marker-alt" className="pr-2" />
+                      MI, 48184 USA
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <MDBIcon icon="phone" className="pr-2" /> 313-457-3323
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <MDBIcon icon="envelope" className="pr-2" />
+                      nnismithllc@gmail.com
+                    </p>
+                  </li>
+                </ul>
+                <hr className="hr-light my-4" />
+                <ul className="list-inline text-center list-unstyled">
+                  <li className="list-inline-item">
+                    <a href="#!" className="p-2 fa-lg w-ic">
+                      <MDBIcon fab icon="twitter" />
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#!" className="p-2 fa-lg w-ic">
+                      <MDBIcon fab icon="linkedin-in" />
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#!" className="p-2 fa-lg w-ic">
+                      <MDBIcon fab icon="instagram" />
+                    </a>
+                  </li>
+                </ul>
+              </MDBCardBody>
+            </MDBCol>
+          </MDBRow>
+        </MDBCard>
+      </section>
+      </form>
+      </div>
+      </div>
+    );
+  }
+
+  
 
 
-// Export for Contact Page
-export default Contact;
+     
--- src/Component/Portfolio.js
+++ src/Component/Portfolio.js
@@ -1,137 +1,151 @@
 import React from "react";
-import {MDBRow, MDBCol, MDBCardBody, MDBIcon, MDBBtn, MDBView, MDBMask } from "mdbreact";
-import BurgerPic from '../Component/public/images/Burger.JPG';
-import LandPic from '../Component/public/images/LandDegrees.JPG';
-import NotePic from '../Component/public/images/NoteTaker.JPG';
-import SnapPic from '../Component/public/images/snapbudget.JPG';
+import {
+  MDBRow,
+  MDBCol,
+  MDBCardBody,
+  MDBIcon,
+  MDBBtn,
+  MDBView,
+  MDBMask,
+} from "mdbreact";
+import BurgerPic from "../Component/public/images/Burger.JPG";
+import LandPic from "../Component/public/images/LandDegrees.JPG";
+import NotePic from "../Component/public/images/NoteTaker.JPG";
+import SnapPic from "../Component/public/images/snapbudget.JPG";
 
-
-function Portfolio () {
+function Portfolio() {
   return (
     <section className="text-center my-5">
-        <h2 className="h1-responsive font-weight-bold my-5">
+      <h2 className="h1-responsive font-weight-bold my-5">
         Check out some of My Creative Framework Applications.
-        </h2>
-        <p className="grey-text w-responsive mx-auto mb-5">
-         </p>
+      </h2>
+      <p className="grey-text w-responsive mx-auto mb-5"></p>
 
-        <MDBRow className="d-flex justify-content-center">
-          <MDBCol md="6" xl="5" className="mb-4">
-            <MDBView className="overlay rounded z-depth-2" waves>
-              <img src={LandPic} alt="LandDegree" className="img-fluid"/>
-              <a href="https://nnismithllc.github.io/LandDegrees/">
-                <MDBMask overlay="white-slight" />
-              </a>
-            </MDBView>
-            <MDBCardBody className="pb-0">
-              <a href="https://github.com/nnismithllc/LandDegrees" className="green-text">
-                <h5 className="font-weight-bold mt-2 mb-3">
+      <MDBRow className="d-flex justify-content-center">
+        <MDBCol md="6" xl="5" className="mb-4">
+          <MDBView className="overlay rounded z-depth-2" waves>
+            <img src={LandPic} alt="LandDegree" className="img-fluid" />
+            <a href="https://nnismithllc.github.io/LandDegrees/">
+              <MDBMask overlay="white-slight" />
+            </a>
+          </MDBView>
+          <MDBCardBody className="pb-0">
+            <a
+              href="https://github.com/nnismithllc/LandDegrees"
+              className="green-text"
+            >
+              <h5 className="font-weight-bold mt-2 mb-3">
                 <MDBIcon icon="umbrella-beach" className="pr-2" />
                 LandDegrees
-                </h5>
-              </a>
-              <h4 className="font-weight-bold mb-3">Explore Your City's Weather</h4>
-              <p>
-                Simple application with astounding visual aspects to capture the essence of color in one of the earth's beautiful landmarks. This application allows you to search the weather in your area by city and state.
-              </p>
-              <MDBBtn color="success" rounded>
-                GitHub Link
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCol>
-          <MDBCol md="6" xl="5" className="mb-4">
-            <MDBView className="overlay rounded z-depth-2" waves>
-              <img
-                src= {BurgerPic} alt="Burger" className="img-fluid"/>
-              <a href="https://burgermebabydelight.herokuapp.com/">
-                <MDBMask overlay="white-slight" />
-              </a>
-            </MDBView>
-            <MDBCardBody className="pb-0">
-              <a href="https://github.com/nnismithllc/EatDaBurger" className="blue-text">
-                <h5 className="font-weight-bold mt-2 mb-3">
+              </h5>
+            </a>
+            <h4 className="font-weight-bold mb-3">
+              Explore Your City's Weather
+            </h4>
+            <p>
+              Simple application with astounding visual aspects to capture the
+              essence of color in one of the earth's beautiful landmarks. This
+              application allows you to search the weather in your area by city
+              and state.
+            </p>
+            <MDBBtn color="success" rounded>
+              GitHub Link
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCol>
+        <MDBCol md="6" xl="5" className="mb-4">
+          <MDBView className="overlay rounded z-depth-2" waves>
+            <img src={BurgerPic} alt="Burger" className="img-fluid" />
+            <a href="https://burgermebabydelight.herokuapp.com/">
+              <MDBMask overlay="white-slight" />
+            </a>
+          </MDBView>
+          <MDBCardBody className="pb-0">
+            <a
+              href="https://github.com/nnismithllc/EatDaBurger"
+              className="blue-text"
+            >
+              <h5 className="font-weight-bold mt-2 mb-3">
                 <MDBIcon icon="hamburger" className="pr-2" />
-                  EatDaBurger
-                </h5>
-              </a>
-              <h4 className="font-weight-bold mb-3">Let's Order a Burger</h4>
-              <p>
-                This application allows you to name a unique burger for you to move from a "order" status to a "devour" state with button functionality. 
-                The application allows users to use the "CRUD" method to interact with the data platform.
-              </p>
-              <MDBBtn color="success" rounded>
-                GitHub Link
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCol>
-          <MDBCol md="6" xl="5" className="mb-4">
-            <MDBView className="overlay rounded z-depth-2" waves>
-              <img src={NotePic} alt="Note Taker" className="img-fluid" />
-              <a href="https://notetak3r.herokuapp.com/">
-                <MDBMask overlay="white-slight" />
-              </a>
-            </MDBView>
-            <MDBCardBody className="pb-0">
-              <a href="https://github.com/nnismithllc/NoteTak3r" className="pink-text">
-                <h5 className="font-weight-bold mt-2 mb-3">
-                  <MDBIcon icon="sticky-note" className="pr-2" />
-                  Note Taker
-                </h5>
-              </a>
-              <h4 className="font-weight-bold mb-3">Take Notes and Save Information</h4>
-              <p>
-                This application allows you to transition from home page to note page by button functionality. The note page allows users to enter notes and save on their local drive for later use. App uses the "CRUD" method to engage users to create, read, update, and delete entries in order of submission. 
-              </p>
-              <MDBBtn color="success" rounded>
-               GitHub Link
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCol>
-          <MDBCol md="6" xl="5" className="mb-4">
-            <MDBView className="overlay rounded z-depth-2" waves>
-              <img
-                src={SnapPic} alt="SnapBudget" className="img-fluid" />
-              <a href="https://mysnapbudget.herokuapp.com/">
-                <MDBMask overlay="white-slight" />
-              </a>
-            </MDBView>
-            <MDBCardBody className="pb-0">
-              <a href="https://github.com/WD40JS/project_2" className="cyan-text">
-                <h5 className="font-weight-bold mt-2 mb-3">
-                  <MDBIcon icon="camera-retro" className="pr-2" />
-                  Snap Budget
-                </h5>
-              </a>
-              <h4 className="font-weight-bold mb-3">Keep Record of Your Income</h4>
-              <p>
-                This application is a mysql data database for users who want to track their income. The application use a login system that allows privacy of entry data for amount gain, loss, and keeps record of entries. The application also displays a graph to show the marketing trend for each customer spending or gain entry.
-              </p>
-              <MDBBtn color="success" rounded>
-                GitHub Link
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCol>
-        </MDBRow>
-      </section>
+                EatDaBurger
+              </h5>
+            </a>
+            <h4 className="font-weight-bold mb-3">Let's Order a Burger</h4>
+            <p>
+              This application allows you to name a unique burger for you to
+              move from a "order" status to a "devour" state with button
+              functionality. The application allows users to use the "CRUD"
+              method to interact with the data platform.
+            </p>
+            <MDBBtn color="success" rounded>
+              GitHub Link
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCol>
+        <MDBCol md="6" xl="5" className="mb-4">
+          <MDBView className="overlay rounded z-depth-2" waves>
+            <img src={NotePic} alt="Note Taker" className="img-fluid" />
+            <a href="https://notetak3r.herokuapp.com/">
+              <MDBMask overlay="white-slight" />
+            </a>
+          </MDBView>
+          <MDBCardBody className="pb-0">
+            <a
+              href="https://github.com/nnismithllc/NoteTak3r"
+              className="pink-text"
+            >
+              <h5 className="font-weight-bold mt-2 mb-3">
+                <MDBIcon icon="sticky-note" className="pr-2" />
+                Note Taker
+              </h5>
+            </a>
+            <h4 className="font-weight-bold mb-3">
+              Take Notes and Save Information
+            </h4>
+            <p>
+              This application allows you to transition from home page to note
+              page by button functionality. The note page allows users to enter
+              notes and save on their local drive for later use. App uses the
+              "CRUD" method to engage users to create, read, update, and delete
+              entries in order of submission.
+            </p>
+            <MDBBtn color="success" rounded>
+              GitHub Link
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCol>
+        <MDBCol md="6" xl="5" className="mb-4">
+          <MDBView className="overlay rounded z-depth-2" waves>
+            <img src={SnapPic} alt="SnapBudget" className="img-fluid" />
+            <a href="https://mysnapbudget.herokuapp.com/">
+              <MDBMask overlay="white-slight" />
+            </a>
+          </MDBView>
+          <MDBCardBody className="pb-0">
+            <a href="https://github.com/WD40JS/project_2" className="cyan-text">
+              <h5 className="font-weight-bold mt-2 mb-3">
+                <MDBIcon icon="camera-retro" className="pr-2" />
+                Snap Budget
+              </h5>
+            </a>
+            <h4 className="font-weight-bold mb-3">
+              Keep Record of Your Income
+            </h4>
+            <p>
+              This application is a mysql data database for users who want to
+              track their income. The application use a login system that allows
+              privacy of entry data for amount gain, loss, and keeps record of
+              entries. The application also displays a graph to show the
+              marketing trend for each customer spending or gain entry.
+            </p>
+            <MDBBtn color="success" rounded>
+              GitHub Link
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCol>
+      </MDBRow>
+    </section>
   );
 }
 
-export default Portfolio
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default Portfolio;
