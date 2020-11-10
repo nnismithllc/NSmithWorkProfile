2ebe42024d61efa8b2335362ae59b89590f3e8cc
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-11-10 14:57:15 -0500

    updated css file - working app

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -8,36 +8,38 @@ import { NavHashLink } from "react-router-hash-link";
 function Navbar() {
   return (
     <BrowserRouter>
-      <MDBNav color="purple" className="font-weight-bold py-4 px-2 mb-4">
+      <MDBNav color="purple" className="font-weight-bold py-4 px-4 mb-6">
         <NavHashLink
           className="yellow-text"
           MDBIcon
           icon="home"
           active
-          to="#home"
-        >
+          to="#home"> 
           Home
         </NavHashLink>
-        <NavHashLink className="blue-text" MDBIcon icon="female" to="#About">
+
+        <NavHashLink 
+        className="blue-text" 
+        MDBIcon icon="female" 
+        to="#About">
           About Me
         </NavHashLink>
+       
         <NavHashLink
           className="green-text"
           MDBIcon
           icon="laptop"
-          to="#Portfolio"
-        >
+          to="#Portfolio">
           My Portfolio
         </NavHashLink>
+
         <NavHashLink
           className="orange-text"
-          MDBIcon
-          far
-          icon="envelope-open"
-          to="#Contact"
-        >
+          MDBIcon icon="envelope-open"
+          to="#Contact">
           Contact Me
         </NavHashLink>
+        
       </MDBNav>
     </BrowserRouter>
   );
